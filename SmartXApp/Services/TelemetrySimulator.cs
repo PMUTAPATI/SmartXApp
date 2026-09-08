2fb9c914bcb22bc1c6cecf700837611dc94e495a
primmyt19-cpu <104351053+PMUTAPATI@users.noreply.github.com>
2026-09-08 14:08:41 +0200

    Update TelemetrySimulator.cs
    Data

--- SmartXApp/Services/TelemetrySimulator.cs
+++ SmartXApp/Services/TelemetrySimulator.cs
@@ -13,7 +13,7 @@ namespace SmartXApp.Services
             var isSpike = rng.Next(1, 100) <= 15;
             var power = isSpike ? rng.Next(1500, 3000) : rng.Next(100, 600);
 
-            // Assign
+            // Assign wattage
             return new TelemetryData
             {
                 DeviceId = deviceId,
